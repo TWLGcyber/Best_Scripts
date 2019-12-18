2caf10f935d736794d96e7dc7ad4774800b3a8cc
TWLGcyber <34041366+TWLGcyber@users.noreply.github.com>
2019-12-18 07:59:31 -0500

    Create California.sh

---
+++ California.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Runs a config script
+sudo chmod 777 config.sh
+
+# Runs updates
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Reconfigures desktop
+sudo apt-get install ubuntu-desktop
+
+# 
